f47438c4aba66a085e05cc5e9267f6a4067930d4
OcaenEyes <1450136519@qq.com>
2021-02-23 00:19:21 +0800

    master

---
+++ TF/tf-bert-learn/model_server/modelserver.sh
@@ -0,0 +1,14 @@
+echo '正在启动bert model_server'
+cd /Users/gaozhiyong/Documents/GitHub/ML/TF/tf-bert-learn/model_server
+sudo rm -rf tmp*
+
+bert-base-serving-start \
+    -model_dir /Users/gaozhiyong/Documents/GitHub/ML/TF/tf-bert-learn/output/comment_1 \
+    -bert_model_dir /Users/gaozhiyong/Documents/GitHub/ML/TF/tf-bert-learn/uncased_L-2_H-128_A-2 \
+    -model_pb_dir /Users/gaozhiyong/Documents/GitHub/ML/TF/tf-bert-learn/output/comment_1 \
+    -mode CLASS \
+    -max_seq_len 128 \
+    -http_port 8091 \
+    -port 5575 \
+    -port_out 5576 \
+    -device_map 1
