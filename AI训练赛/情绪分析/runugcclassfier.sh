fb4bdfe5b2c7d79489ef3c320c6d874250715fff
OcaenEyes <1450136519@qq.com>
2021-05-23 09:16:01 +0800

    master

--- AI训练赛/情绪分析/runugcclassfier.sh
+++ AI训练赛/情绪分析/runugcclassfier.sh
@@ -15,3 +15,20 @@ sudo python run_ugcclassifier.py \
 --learning_rate=2e-5 \
 --num_train_epochs=5.0 \
 --output_dir=$OUTPUT_DIR
+
+# win 命令
+python run_ugcclassifier.py ^
+--task_name=ugc ^
+--do_train=true ^
+--do_predict=true ^
+--vocab_file=D:\GitHub\ML\AI训练赛\情绪分析\model\chinese_L-12_H-768_A-12\vocab.txt ^
+--bert_config_file=D:\GitHub\ML\AI训练赛\情绪分析\model\chinese_L-12_H-768_A-12\bert_config.json ^
+--data_dir=D:\GitHub\ML\AI训练赛\情绪分析\data ^
+--init_checkpoint=D:\GitHub\ML\AI训练赛\情绪分析\model\chinese_L-12_H-768_A-12\bert_model.ckpt ^
+--max_seq_length=128 ^
+--train_batch_size=10 ^
+--learning_rate=2e-5 ^
+--num_train_epochs=5.0 ^
+--output_dir=D:\GitHub\ML\AI训练赛\情绪分析\output
+
+#D:\\GitHub\\ML\\AI训练赛\\情绪分析\\output\\ugc\\versions
